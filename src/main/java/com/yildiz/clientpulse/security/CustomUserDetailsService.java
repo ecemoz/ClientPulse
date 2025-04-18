ee24665fb9e910ec71630dd1421aa78ac070d47f
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2025-04-18 23:46:26 +0300

    CustomUserDetailsService is added

---
+++ src/main/java/com/yildiz/clientpulse/security/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.yildiz.clientpulse.security;
+
+import com.yildiz.clientpulse.models.User;
+import com.yildiz.clientpulse.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPassword())
+                .authorities("ROLE_" + user.getRole().name())
+                .build();
+    }
+}
--- src/main/java/com/yildiz/clientpulse/security/JwtFilter.java
+++ src/main/java/com/yildiz/clientpulse/security/JwtFilter.java
@@ -1,4 +1,5 @@
 package com.yildiz.clientpulse.security;
+
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
