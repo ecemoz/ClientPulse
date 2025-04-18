df1397a0e681317e87597d11e271e81f1021ddff
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2025-04-18 22:42:54 +0300

    user model is opened

---
+++ src/main/java/com/yildiz/clientpulse/ClientPulseApplication.java
@@ -0,0 +1,13 @@
+package com.yildiz.clientpulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientPulseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientPulseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yildiz/clientpulse/ClientPulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yildiz.clientpulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientPulseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
