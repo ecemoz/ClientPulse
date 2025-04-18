88e542ca1108f2e531fbc9976a703344716b4f8c
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2025-04-19 00:03:03 +0300

    security config is fixed

--- src/main/java/com/yildiz/clientpulse/security/SecurityConfig.java
+++ src/main/java/com/yildiz/clientpulse/security/SecurityConfig.java
@@ -1,13 +1,9 @@
 package com.yildiz.clientpulse.security;
 
-import com.yildiz.clientpulse.security.JwtFilter;
-import com.yildiz.clientpulse.security.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -26,14 +22,15 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(org.springframework.security.config.annotation.web.builders.HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http
+                .securityMatcher("/**") // tüm istekler için geçerli
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**", "/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
                 )
-                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
-        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
