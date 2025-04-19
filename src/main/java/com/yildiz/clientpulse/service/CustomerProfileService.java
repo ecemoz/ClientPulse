06c0d47fb92f11fbc9d77c2d01c0c4c990c35f0f
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2025-04-19 05:36:50 +0300

    CustomerProfileService is created.

---
+++ src/main/java/com/yildiz/clientpulse/service/CustomerProfileService.java
@@ -0,0 +1,11 @@
+package com.yildiz.clientpulse.service;
+
+import com.yildiz.clientpulse.models.CustomerProfile;
+import com.yildiz.clientpulse.models.User;
+import java.util.List;
+
+public interface CustomerProfileService {
+    CustomerProfile createProfileForUser(User user);
+    List<CustomerProfile> getAllProfiles();
+    CustomerProfile getProfileByUserId(Long userId);
+}
