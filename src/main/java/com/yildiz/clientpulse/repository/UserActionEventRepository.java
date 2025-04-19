d37407bea6a3dfc523cbd8716187372cffd3f852
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2025-04-19 03:31:14 +0300

    UserActionEventRepository is updated.

--- src/main/java/com/yildiz/clientpulse/repository/UserActionEventRepository.java
+++ src/main/java/com/yildiz/clientpulse/repository/UserActionEventRepository.java
@@ -3,7 +3,9 @@ package com.yildiz.clientpulse.repository;
 import com.yildiz.clientpulse.models.UserActionEventEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface UserActionEventRepository extends JpaRepository<UserActionEventEntity, Long> {
+    List<UserActionEventEntity> findByUserId(Long userId);
 }
